1a825f89e7b08789cde7d3ef94f8c5304f29669e
Elizabeth Rhee <erhee@db8.cse.nd.edu>
2025-11-03 01:24:19 -0500

    Fixed User login and registration and flash messages

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 # DATABASE CONFIGURATION
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sjacobo3:newpassword@localhost/sjacobo3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.urandom(24) # generate a random 24 byte string for the secret key for session cookies so it is not tampered with by client
 
 # initialize SQLAlechemy
 db = SQLAlchemy(app)
@@ -30,7 +31,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}"        
@@ -91,7 +92,7 @@ def login():
             session['user_id'] = user.id
             session['username'] = user.username
             flash(f"Welcome, {user.username}")
-            return redirect(url_for('home_page'))
+            return redirect(url_for('home'))
         else:
             flash("Invalid username or password.")
             return redirect(url_for('login'))
